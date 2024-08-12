252d3568dab65004181b2692ebd4e12352fd5348
Andrey Perepelykov <bars_skif@mail.ru>
2024-08-13 01:08:16 +0300

    fix

--- src/client/components/OurServicess/index.tsx
+++ src/client/components/OurServicess/index.tsx
@@ -1,17 +1,21 @@
+import { ReactElement } from "react";
+
 import { Box } from "@mantine/core";
 
-import classes from "./OurServicess.module.css";
 import { InteractiveGallery } from "../InteractiveGallery";
 
-export const OurServicess = () => {
+import classes from "./OurServicess.module.css";
+
+export const OurServicess = (): ReactElement => {
   return (
     <Box className={classes.outServicessWrapper}>
       <h2
         style={{
           margin: 0,
+          textTransform: "uppercase",
         }}
       >
-        НАШИ УСЛУГИ
+        наши услуги
       </h2>
       <h3
         style={{
